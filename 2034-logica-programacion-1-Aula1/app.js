512906cf690abdcc749160a6e56d2d553509c323
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-01-15 20:04:15 -0500

    Condicional if, variables, comentarios y camelCase

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,8 +1,14 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt("Me indicas un número, entre 1 y 10 por favor.");
 
 console.log(numeroUsuario);
 
+/*
+    Este código realiza la
+    comparación.
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    alert(`Acertaste el número es : ${numeroUsuario}.`);
+} else {
+    alert('Lo siento, no acertaste el número.');
 }
