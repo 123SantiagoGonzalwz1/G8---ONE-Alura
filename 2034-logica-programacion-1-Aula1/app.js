5ca9bf1b37ea93ac482e2c132099546062840754
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-01-17 20:40:23 -0500

    Usando conceptos como el operador ternario, método Math.random y floor y el parseInt

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,33 +1,92 @@
-let numeroSecreto = 6;
+let numeroMaximoPosible = 10;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximoPosible) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraIntentos = 'intento';
+let maximosIntentos = 3;
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt("Me indicas un número, entre 1 y 10 por favor.");
-
-    console.log(numeroUsuario);
-    console.log(numeroSecreto == numeroUsuario);
+    numeroUsuario = parseInt(prompt(`Me indicas un número, entre 1 y ${numeroMaximoPosible} por favor.`));
 
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste el número es : ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraIntentos}.`);
+        alert(`Acertaste el número es : ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'intento' : 'intentos'}.`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor.');
         } else {
             alert('El número secreto es mayor.');
         }
-        intentos += 1;
-        palabraIntentos = 'intentos';
 
-        if (intento > 3) {
-            alert('Llegaste al número máxio de intentos');
+        intentos++;
+
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máxio de ${maximosIntentos} intentos`);
             break;
         }
 
         alert('Lo siento, no acertaste el número.');
     }
-} 
+}  
+
+/* console.log("¡Bienvenidos Usuarios!");
+
+let name = "Santiago González";
+console.log(`¡Hola ${name}!`);
+
+let nombre = "Santiago González";
+alert(`¡Hola ${nombre}!`);
+
+let lenguajePreferido = prompt("¿Cuál es el lenguaje de programación que más te gusta?");
+console.log(lenguajePreferido);
+
+let valor1 = 10;
+let valor2 = 30;
+let resultado = valor1 + valor2;
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}`);
+
+let value1 = 25.0;
+let value2 = 23.0;
+let result = value1 - value2;
+console.log(`La diferencia entre ${value1} y ${value2} es igual a ${result}`);
+
+let age = parseInt(prompt("Por favor, ingresa tu edad: "));
+
+if (age >= 18) {
+    console.log('¡Eres mayor de edad!');
+} else {
+    console.log('¡Eres menor de edad!');
+}
+
+let numero = parseInt(prompt("Por favor, ingresa un número: "));
+
+if (numero > 0) {
+    alert('El número es positivo.');
+} else if (numero < 0) {
+    alert('El número es negativo.');
+} else {
+    alert('El número es cero.');
+}
+
+let number = 1;
+while (number <= 10) {
+    console.log(`Número ${number}`);
+    number++;
+}
+
+let nota = 2;
+if (nota >= 7) {
+    console.log("Aprobado.");
+} else {
+    console.log("Reprobado");
+}
+
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+console.log(numeroAleatorio);
+
+let numeroAleatorio2 = Math.floor(Math.random() * 10) + 1;
+console.log(numeroAleatorio2);
+
+let numeroAleatorio3 = Math.floor(Math.random() * 1000) + 1;
+console.log(numeroAleatorio3); */
 
 /* let edad = 20;
 let tieneLicencia = true;
