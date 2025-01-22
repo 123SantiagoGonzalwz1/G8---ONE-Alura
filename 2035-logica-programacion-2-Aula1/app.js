7cbbb3728be77ef1a3532455a7fcffbc7a70d017
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-01-21 20:05:22 -0500

    Arrays, push, length, indice de lista, agregar y remover un atributo

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = generarNumeroAleatorio();
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -12,8 +13,38 @@ function generarNumeroAleatorio() {
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}.`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor.');
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor.');
+        }
+        intentos++;
+        limpiarCaja();
+    }
     return;
 }
 
-asignarTextoElemento('h1', '¡Juego del número secreto!');
-asignarTextoElemento('p', 'Indica un número del 1 al 10.');
+function limpiarCaja() {
+    document.getElementById('valorUsuario').value = '';
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', '¡Juego del número secreto!');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10.');
+    numeroSecreto = generarNumeroAleatorio();
+    intentos = 1;
+    return;
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
--- js-curso-2-main/app.js
+++ js-curso-2-main/app.js
@@ -1,7 +1,99 @@
+function mostrarTablaDeMultiplicar(numero) {
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        console.log(`${numero} x ${i} = ${resultado}`);
+    }
+}
+
+let numero = 7;
+mostrarTablaDeMultiplicar(numero);
+
+/* const PI = 3.14
+
+function areaSalaCircular(radio) {
+    let area = PI * radio * radio;
+    return area;
+}
+
+function perimetroSalaCircular(radio) {
+    let perimetro = 2 * PI * radio;
+    return perimetro;
+}
+
+let radio = 25.5;
+let area = areaSalaCircular(radio);
+let perimetro = perimetroSalaCircular(radio);
+console.log(`El área de la sala circular es ${area} y su perímetro es ${perimetro}.`); */
+
+/* function areaSalaRectangular(altura, anchura) {
+    let area = altura * anchura;
+    return area;
+}
+
+function perimetroSalaRectangular(altura, anchura) {
+    let perimetro = 2 * (altura + anchura);
+    return perimetro;
+}
+
+let altura = 5;
+let anchura = 10;
+let area = areaSalaRectangular(altura, anchura);
+let perimetro = perimetroSalaRectangular(altura, anchura);
+
+console.log(`El área de la sala rectangular es ${area} y su perímetro es ${perimetro}.`); */
+
+/* function dolaresAPesosColombianos(dolares) {
+    let cotizacionDolar = 4.285;
+    let pesos = dolares * cotizacionDolar;
+    return pesos;
+}
+
+let valorDolar = 50;
+let valorEnPesos = dolaresAPesosColombianos(valorDolar);
+console.log(`${valorDolar} dólares es $${valorEnPesos} pesos colombianos.`); */
+
+
+/* function calculoIndiceMasaCorporal(altura, peso) {
+    let indice = peso / (altura * altura);
+    return console.log(`Tu IMC es ${indice}`);
+}
+
+calculoIndiceMasaCorporal(1.75, 70);
+
+function calcularFactorial(numero) {
+    if (numero === 0 || numero === 1) {
+        return 1;
+    } else {
+        return numero * calcularFactorial(numero - 1);
+    }
+}
+
+let numero = 5;
+let resultado = calcularFactorial(numero);
+console.log(`El factorial de ${numero} es ${resultado}.`); */
 
 
 
-function mensajeConsola() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function mensajeConsola() {
     return console.log('¡Hola Mundo!');
 }
 
@@ -38,7 +130,7 @@ numeroMayor(4, 10);
 dobleDelNumero(30);
 promedioNumero(5, 2, 3);
 mensajeNombre('Santiago');
-mensajeConsola();
+mensajeConsola(); */
 
 
 
