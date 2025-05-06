7876b0ca3f9b2316ea83b15c44860c17ae34b287
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-05-05 21:23:44 -0500

    Aprendiendo más sobre excepciones y la clase IO

--- screenmatch/src/com/aluracursos/screenmatch/models/Titulo.java
+++ screenmatch/src/com/aluracursos/screenmatch/models/Titulo.java
@@ -1,5 +1,8 @@
 package com.aluracursos.screenmatch.models;
 
+import com.aluracursos.screenmatch.exception.ErrorEnConversionDeDuracionException;
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
     private String nombre;
     private int fechaDeLanzamiento;
@@ -11,6 +14,16 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nombre, int fechaDeLanzamiento) {
         this.nombre = nombre;
         this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public Titulo(TituloOmdb miTituloOmdb) {
+        this.nombre = miTituloOmdb.title();
+        this.fechaDeLanzamiento = Integer.valueOf(miTituloOmdb.year());
+        if(miTituloOmdb.runtime().contains("N/A")) {
+            throw new ErrorEnConversionDeDuracionException("No pude convertir la duración," +
+                    "porque contiene un N/A");
+        }
+        this.duracionEnMinutos = Integer.valueOf(miTituloOmdb.runtime().substring(0, 3).replace(" ", ""));
     }
 
     public String getNombre() {
@@ -67,5 +80,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return "(Nombre= '" + nombre + '\'' +
+                ", Fecha De Lanzamiento= " + fechaDeLanzamiento +
+                ", Duración en Minutos= " + duracionEnMinutos + ")";
     }
 }
