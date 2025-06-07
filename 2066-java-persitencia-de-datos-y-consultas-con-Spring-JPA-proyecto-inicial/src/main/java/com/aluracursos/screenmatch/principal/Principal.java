185ef5f819aca687d3726586931e9ab83ee863bc
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-06-07 17:15:57 -0500

    Elimina archivo con clave secreta

--- 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.principal;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.model.Serie;
+import com.aluracursos.screenmatch.repository.ISerieRepository;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import java.util.ArrayList;
@@ -15,9 +16,14 @@ public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=dc2c4dc8";
+    private final String API_KEY = "&apikey=" + System.getenv("API_KEY_OMDB");
     private ConvierteDatos conversor = new ConvierteDatos();
     private List<DatosSerie> datosSeries = new ArrayList<>();
+    private ISerieRepository repositorio;
+
+    public Principal(ISerieRepository repository) {
+        this.repositorio = repository;
+    }
 
     public void muestraElMenu() {
         var opcion = -1;
@@ -74,15 +80,14 @@ public class Principal {
     }
     private void buscarSerieWeb() {
         DatosSerie datos = getDatosSerie();
-        datosSeries.add(datos);
+        Serie serie = new Serie(datos);
+        repositorio.save(serie);
+        // datosSeries.add(datos);
         System.out.println(datos);
     }
 
     private void mostrarSeriesBuscadas() {
-        List<Serie> series = new ArrayList<>();
-        series = datosSeries.stream()
-                .map(Serie::new)
-                .collect(Collectors.toList());
+        List<Serie> series = repositorio.findAll();
 
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
