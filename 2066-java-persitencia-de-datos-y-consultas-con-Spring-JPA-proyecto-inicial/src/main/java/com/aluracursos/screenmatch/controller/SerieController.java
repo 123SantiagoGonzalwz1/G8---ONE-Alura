cfee75d3b66084260319001512a3db9f8b052d87
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-06-18 21:40:42 -0500

    Aprendiendo sobre Cors y DTO

--- 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ 2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,26 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repository.ISerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private ISerieRepository repository;
 
     @GetMapping("/series")
-    public String mostrarMensaje() {
-        return "Primer mensaje";
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return repository.findAll().stream().
+                map(s -> new SerieDTO(
+                        s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(),
+                        s.getPoster(), s.getGenero(), s.getActores(), s.getSinopsis()
+                ))
+                .collect(Collectors.toList());
     }
 }
