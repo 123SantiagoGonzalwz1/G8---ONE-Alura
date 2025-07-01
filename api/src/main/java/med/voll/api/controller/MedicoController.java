a1e57ad75c78b391002ef1cdbe88571eec8f76e2
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-06-30 21:50:54 -0500

    Listando medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListaMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.IMedicoRepository;
 import med.voll.api.medico.Medico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +24,10 @@ public class MedicoController {
     @PostMapping
     public void registrar(@RequestBody @Valid DatosRegistroMedico datos) {
         repository.save(new Medico(datos));
+    }
+
+    @GetMapping
+    public Page<DatosListaMedico> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion) {
+        return repository.findAll(paginacion).map(DatosListaMedico::new);
     }
 }
