bac03e9132e3bc7517ef8433a412a2254dc682b6
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-06-30 20:21:45 -0500

    Usando Bean Validation

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.IMedicoRepository;
+import med.voll.api.medico.Medico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+    @Autowired
+    private IMedicoRepository repository;
+
+    @Transactional
     @PostMapping
-    public void registrar(@RequestBody DatosRegistroMedico datos) {
-        System.out.println(datos);
+    public void registrar(@RequestBody @Valid DatosRegistroMedico datos) {
+        repository.save(new Medico(datos));
     }
 }
