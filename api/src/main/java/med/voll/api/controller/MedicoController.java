e5b157269db3e45a592231d65234755bb303a7e4
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-07-01 19:22:45 -0500

    Agregando métodos put y delete

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListaMedico;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.IMedicoRepository;
-import med.voll.api.medico.Medico;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,6 +25,20 @@ public class MedicoController {
 
     @GetMapping
     public Page<DatosListaMedico> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion) {
-        return repository.findAll(paginacion).map(DatosListaMedico::new);
+        return repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
+    }
+
+    @Transactional
+    @PutMapping
+    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos) {
+        var medico = repository.getReferenceById(datos.id());
+        medico.actualizarInformaciones(datos);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.eliminar();
     }
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -17,6 +17,7 @@ public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Boolean activo;
     private String nombre;
     private String email;
     private String telefono;
@@ -31,11 +32,28 @@ public class Medico {
 
     public Medico(DatosRegistroMedico datos) {
         this.id = null;
+        this.activo = true;
         this.nombre = datos.nombre();
         this.email = datos.email();
         this.telefono = datos.telefono();
         this.documento = datos.documento();
         this.especialidad = datos.especialidad();
         this.direccion = new Direccion(datos.direccion());
+    }
+
+    public void actualizarInformaciones(DatosActualizacionMedico datos) {
+        if (datos.nombre() != null) {
+            this.nombre = datos.nombre();
+        }
+        if (datos.telefono() != null) {
+            this.telefono = datos.telefono();
+        }
+        if (datos.direccion() != null) {
+            this.direccion.actualizarDireccion(datos.direccion());
+        }
+    }
+
+    public void eliminar() {
+        this.activo = false;
     }
 }
