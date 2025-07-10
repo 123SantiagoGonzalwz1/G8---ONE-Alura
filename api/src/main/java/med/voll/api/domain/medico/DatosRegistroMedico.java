469efd15010b7441f6c8b07fea5c9bc4ac8dc3d0
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-07-09 21:05:56 -0500

    Configuraciones iniciales de Spring security

--- api/src/main/java/med/voll/api/domain/medico/DatosRegistroMedico.java
+++ api/src/main/java/med/voll/api/domain/medico/DatosRegistroMedico.java
@@ -8,11 +8,25 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(
-        @NotBlank String nombre,
-        @NotBlank @Email String email,
-        @NotBlank String telefono,
-        @NotBlank @Pattern(regexp = "\\d{7,10}") String documento,
-        @NotNull Especialidad especialidad,
-        @NotNull @Valid DatosDireccion direccion
+        @NotBlank(message = "{nombre.obligatorio}")
+        String nombre,
+
+        @NotBlank(message = "{email.obligatorio}")
+        @Email(message = "{email.invalido}")
+        String email,
+
+        @NotBlank(message = "{telefono.obligatorio}")
+        String telefono,
+
+        @NotBlank(message = "{documento.obligatorio}")
+        @Pattern(regexp = "\\d{7,10}", message = "{documento.invalido}")
+        String documento,
+
+        @NotNull(message = "{especialidad.obligatorio}")
+        Especialidad especialidad,
+
+        @NotNull(message = "{direccion.obligatorio}")
+        @Valid
+        DatosDireccion direccion
 ) {
 }
