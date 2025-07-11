2ba6cff151287343cba58afaa2231a965707b4d5
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-07-10 19:30:15 -0500

    Usando Json Web Toke

--- api/src/main/java/med/voll/api/domain/medico/Medico.java
+++ api/src/main/java/med/voll/api/domain/medico/Medico.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.domain.direccion.Direccion;
 
+import java.util.Objects;
+
 @Table(name = "medicos")
 @Entity(name = "Medico")
 @Getter
@@ -30,6 +32,22 @@ public class Medico {
     @Embedded
     private Direccion direccion;
 
+    // Constructor sin argumentos
+    public Medico() {}
+
+    // Constructor con todos los campos
+    public Medico(Long id, Boolean activo, String nombre, String email, String telefono, String documento, Especialidad especialidad, Direccion direccion) {
+        this.id = id;
+        this.activo = activo;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.documento = documento;
+        this.especialidad = especialidad;
+        this.direccion = direccion;
+    }
+
+    // Constructor con DatosRegistroMedico
     public Medico(DatosRegistroMedico datos) {
         this.id = null;
         this.activo = true;
@@ -39,6 +57,38 @@ public class Medico {
         this.documento = datos.documento();
         this.especialidad = datos.especialidad();
         this.direccion = new Direccion(datos.direccion());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
     }
 
     public void actualizarInformaciones(DatosActualizacionMedico datos) {
@@ -55,5 +105,18 @@ public class Medico {
 
     public void eliminar() {
         this.activo = false;
+    }
+
+    // Equals y hashCode usando solo id
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Medico medico)) return false;
+        return Objects.equals(id, medico.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- api/src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -5,6 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -12,10 +19,81 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String contrasena;
+
+    // Constructor sin argumentos
+    public Usuario() {
+    }
+
+    // Constructor con todos los campos
+    public Usuario(Long id, String login, String contrasena) {
+        this.id = id;
+        this.login = login;
+        this.contrasena = contrasena;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // equals() y hashCode() usando solo id
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
